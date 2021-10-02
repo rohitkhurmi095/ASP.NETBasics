0664d609886466523782993c93841e6f8aa231d5
rohit <rohit.khurmi095@gmail.com>
2021-10-02 14:28:11 +0530

    Adding Attribute Routing (instead of Url Routing)

--- ASP.NETWebApp/ASP.NETWebApp/App_Start/RouteConfig.cs
+++ ASP.NETWebApp/ASP.NETWebApp/App_Start/RouteConfig.cs
@@ -13,11 +13,18 @@ namespace ASP.NETWebApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            //ATTRIBUTE ROUING
+            //=================
+            routes.MapMvcAttributeRoutes();
+
+
+            //URL ROUTING (DEFAULT = Home/Index)
+            //=============
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
         }
     }
 }
--- ASP.NETWebApp/ASP.NETWebApp/Controllers/HomeController.cs
+++ ASP.NETWebApp/ASP.NETWebApp/Controllers/HomeController.cs
@@ -8,22 +8,34 @@ namespace ASP.NETWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        //Home
+        //=====
+        [Route("Home/Index")]
+        [Route("")]
         public ActionResult Index()
         {
             return View();
         }
 
+        //About
+        //======
+        [Route("Home/About")]
         public ActionResult About()
         {
             return View();
         }
 
+        //Contact
+        //=========
+        [Route("Home/Contact")]
         public ActionResult Contact()
         {
             return View();
         }
 
+        //PROFILE
+        //=========
+        [Route("UserProfile")]
         public ActionResult UserProfile()
         {
             return View();
